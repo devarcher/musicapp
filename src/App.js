694478042639ee21e9c482990048882ebf6d7cfe
devarcher <archershot@hotmail.com>
2020-01-21 13:03:30 -0600

    added basic app bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./App.css";
 
 class App extends React.Component {
   render() {
-    return 'hi';
+    return (
+      <div>
+        <div>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+              ></IconButton>
+              <MenuIcon />
+              <Typography variant="h6">Music App</Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </div>
+    );
   }
 }
 
