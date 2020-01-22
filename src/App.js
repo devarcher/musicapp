75b395dc6d8ec4509143476c225227744b6a25d6
devarcher <archershot@hotmail.com>
2020-01-22 12:27:40 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Header from "./Layouts/Header";
+import LoginBody from "./Layouts/LoginBody";
+import DashBoard from "./Layouts/DashBoard";
 import Footer from "./Layouts/Footer";
 
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    loggedIn: false
+  };
+
   render() {
+    const { loggedIn } = this.state
     return (
       <div>
-        <Header />
+        <Header loggedIn={ loggedIn }/>
+          <div>
+            {loggedIn ? <DashBoard /> : <LoginBody />}
+        </div>
         <Footer />
       </div>
     );
