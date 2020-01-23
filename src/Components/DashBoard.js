bd0c83af0ea251f5acb050a2de142fdc0810478a
devarcher <archershot@hotmail.com>
2020-01-23 15:54:20 -0600

    added switch and color

--- src/Components/DashBoard.js
+++ src/Components/DashBoard.js
@@ -6,8 +6,20 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
-import { createMuiTheme } from "@material-ui/core/styles"
+import { createMuiTheme } from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
 import "typeface-roboto";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#80cbc4"
+    },
+    secondary: {
+      main: "#7FD9BE"
+    }
+  }
+});
 
 const useStyles = makeStyles({
   mainBody: {
@@ -20,6 +32,9 @@ const useStyles = makeStyles({
   card: {
     width: "250px",
     height: "200px"
+  },
+  title: {
+    borderBottom: "1px solid #80cbc4",
   }
 });
 
@@ -27,11 +42,11 @@ const DashBoard = () => {
   const classes = useStyles();
   return (
     <div className={classes.mainBody}>
-      <Grid container spacing={24} justify="center" align="center">
+      <Grid container spacing={24} justify="center">
         <Grid item xs={12} sm={4} lg={3} xl={2}>
           <Card className={classes.card}>
             <CardContent>
-              <Typography component="h2" variant="h6">
+              <Typography component="h2" variant="h6" className={classes.title}>
                 Online Mode
               </Typography>
             </CardContent>
@@ -40,6 +55,12 @@ const DashBoard = () => {
                 Is this application connected to the internet?
               </Typography>
             </CardContent>
+            <CardActions>
+              <ThemeProvider theme={theme}>
+                <Switch
+                ></Switch>
+              </ThemeProvider>
+            </CardActions>
           </Card>
         </Grid>
         <Grid item xs={12} sm={4} lg={3} xl={2}>
