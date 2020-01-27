676c63770b0d3e25b3dc357a821ebc3f8c134dc3
devarcher <archershot@hotmail.com>
2020-01-26 22:38:02 -0600

    added notification handler

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ class App extends React.Component {
     online: true,
     volume: 70,
     quality: "high",
-    notifications: {
-      offline:
-        "Your application is offline. You won't be able to share or stream music to other devices.",
-      volume:
-        "Listening to music at a high volume could cause long-term hearing loss.",
-      quality:
-        "Music quality is degraded. Increase quality if your connection allows it."
-    },
-    notificationsCount: 0
+    notifications: []
+
+    // notifications: {
+    //   offline:
+    //     "Your application is offline. You won't be able to share or stream music to other devices.",
+    //   volume:
+    //     "Listening to music at a high volume could cause long-term hearing loss.",
+    //   quality:
+    //     "Music quality is degraded. Increase quality if your connection allows it."
+    // },
   };
 
   // AppBar Log Out Handler
@@ -36,43 +37,27 @@ class App extends React.Component {
 
   // Dashboard Online Switch Handler
   onlineSwitch = e => {
-    this.setState({ online: !this.state.online }, this.switchCounter);
-  };
-
-  // Switch Counter for Notifications
-  switchCounter = () => {
-    if (this.state.online === false) {
-      this.setState(prevState => ({
-        notificationsCount: prevState.notificationsCount + 1
-      }));
-    } else {
-      this.setState(prevState => ({
-        notificationsCount: 0
-      }));
-    }
+    this.setState({ online: !this.state.online }, this.notificationHandler);
   };
 
   // Dashboard Volume Slider Volume
   sliderVolume = e => {
-    this.setState({ volume: parseInt(e.target.outerText) }, this.volumeCounter);
-  };
-
-  // High Volume Counter for Notifications
-  volumeCounter = () => {
-    if (this.state.volume === 90 || this.state.volume === 100) {
-      this.setState(prevState => ({
-        notificationsCount: prevState.notificationsCount + 1
-      }));
-    } else  {
-      this.setState(prevState => ({
-        notificationsCount: prevState.notifications - 1
-      }));
-    }
+    this.setState({ volume: parseInt(e.target.outerText) }, this.notificationHandler);
   };
 
   // Dashboard Quality Selction
   selectQuality = e => {
     this.setState({ quality: e.target.value });
+  };
+
+  notificationHandler = () => {
+    const { online, volume, quality, notifications } = this.state;
+    if (online === false) {
+      this.setState({ notifications: [...notifications, "z "] });
+    }
+    if (volume > 80) {
+      this.setState({ notifications: [...notifications, "y"] });
+    }
   };
 
   render() {
